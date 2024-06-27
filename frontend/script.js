76855abd4fb435b6a464f1237d42b428e5a4de78
andre-11c <andrechipanarios.11@gmail.com>
2024-06-26 20:47:43 -0500

    config para visualizacion y representacion inicial del grafo

---
+++ frontend/script.js
@@ -0,0 +1,19 @@
+
+//datos para el grafo (archivo .txt)
+const graphData = {
+    nodes: [
+        { id: 'A' },
+        { id: 'B' },
+        { id: 'C' }
+    ],
+    links: [
+        { source: 'A', target: 'B' },
+        { source: 'B', target: 'C' },
+        { source: 'C', target: 'A' }
+    ]
+};
+
+//config para visualizacion 
+const width = 800;
+const height = 600;
+
