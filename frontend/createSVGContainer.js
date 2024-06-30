9759dcf323f1faed182904aba357a480c4dda810
andre-11c <andrechipanarios.11@gmail.com>
2024-06-30 15:37:00 -0500

    Eliminando archivos basura

--- frontend/createSVGContainer.js
+++
@@ -1,7 +0,0 @@
-// Crear el contenedor SVG
-export function createSVGContainer(selector, wigth, height){
-    return d3.select(selector)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-}
